167a7be402f1c094500a32e9b685e70ee2a9cc31
yuragms <yura.gms@gmail.com>
2021-10-15 14:40:05 +0300

    end

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,25 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import {  configureStore, getDefaultMiddleware} from '@reduxjs/toolkit';
 import logger from 'redux-logger';
-import counterReducer from './counter/counter-reducer';
+// import counterReducer from './counter/counter-reducer';
 import contactsReducer from './phonebook/phonebook-reducer';
+import { persistStore, persistReducer, FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER, } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
+const persistConfig = {
+    key: 'contact',
+    storage,
+    blacklist: ['filter'],
+  };
+// const rootReducer =  combineReducers({
+//     contacts: persistReducer(persistConfig, contactsReducer),
+// });
 
+// const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 // const rootReducer =combineReducers({
 //     counter: counterReducer,
@@ -14,15 +30,21 @@ import contactsReducer from './phonebook/phonebook-reducer';
 
 console.log(getDefaultMiddleware());
 
-const middleware = [...getDefaultMiddleware(), logger];
+const middleware = [...getDefaultMiddleware({
+    serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      }
+}), logger];
 
 const store = configureStore({
     reducer: {
-        counter: counterReducer,
-        contacts: contactsReducer
+        contacts: persistReducer(persistConfig, contactsReducer),
     },
     middleware: middleware,
     devTools: process.env.NODE_ENV === 'development', 
+
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export  {store, persistor};
